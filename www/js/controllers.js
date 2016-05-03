a6aed3f40e2e27dc6d155a66bf60d35ee86bf77f
od3n <mr.od3n@gmail.com>
2016-05-03 10:38:06 +0800

    Add Barcode Scanner

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,45 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ['ngCordova'])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $cordovaBarcodeScanner, $http) {
+  $scope.scan = function() {
+    document.addEventListener("deviceready", function () {
+      $scope.scanResults = '';
+      $scope.response = '';
+      $cordovaBarcodeScanner
+      .scan()
+      .then(function(result) {
+        // Success! Barcode data is here
+        // $scope.scanResults = "We got a barcoden" +
+        // "Result: " + result.text + "n" +
+        // "Format: " + result.format + "n" +
+        // "Cancelled: " + result.cancelled;
+
+        // get subject_id first
+        // get student_id from qr // result.text
+        // POST to database
+        $scope.scanResults = result.text;
+        var link = 'http://kembara.my/check.php';
+        $scope.formData = {matapelajaran_id : 12, matrik_no: result.text};
+
+        $http.post(link, $scope.formData).then(function (res) {
+            $scope.response = res.data;
+        });
+      }, function(error) {
+        // An error occurred
+        $scope.scanResults = 'Error: ' + error;
+      });
+    }, false);
+  };
+
+  $scope.clear = function() {
+    $scope.scanResults = '';
+    $scope.response = '';
+  }
+
+  $scope.scanResults = '';
+  $scope.response = '';
+  $scope.formData = {};
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
